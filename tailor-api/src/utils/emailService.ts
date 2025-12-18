f3aac2c179604e2f5947a6e813980d43a7d34ea0
Aleesha Mufthi <aleeshamufthi@gmail.com>
2025-12-18 10:58:23 +0530

    smtp connection issue

--- tailor-api/src/utils/emailService.ts
+++ tailor-api/src/utils/emailService.ts
@@ -3,6 +3,8 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to: string, subject: string, text: string) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,        // Use 587 for TLS, 465 for SSL
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER!,
       pass: process.env.EMAIL_PASS!,
